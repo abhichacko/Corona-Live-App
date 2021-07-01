13115bd2ab1f1ef1251d05eb156913a5b109b8b6
unknown <itsmeabhilashthankachan@gmail.com>
2021-07-01 18:07:10 +0530

    added basic home and topnavbar component

---
+++ src/Home.js
@@ -0,0 +1,15 @@
+import TopNavBar from "./components/TopNavBar";
+import React, { Component } from 'react';
+class Home extends Component {
+    state = {  }
+    render() { 
+        
+        return ( 
+            <div>
+                <TopNavBar />
+            </div>
+         );
+    }
+}
+ 
+export default Home;
---
+++ src/components/TopNavBar.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class TopNavBar extends Component {
+    state = {  }
+    render() { 
+        return ( 
+            <div>
+                <label>top nav
+                </label>
+            </div>
+         );
+    }
+}
+ 
+export default TopNavBar;
